866c014970d9144315b9adf90f0bc7635af2dede
valentinaguti <valentina.gutierrez.gonzalez@alumnos.sip.cl>
2022-04-07 11:46:20 -0400

    coding

---
+++ Gutierrez_valentina_jsprediccion.js
@@ -0,0 +1,21 @@
+function myBirthYearFunc(){
+    console.log("Naci en " + 2006);
+}    
+myBirthYearFunc (1980)
+"Naci en 1980" el console.log me indica en que año Naci 
+
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+myBirthYearFunc(1980)
+"naci en "1980" 
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+console.log(20) me indica el resultado de la suma de los numeros
