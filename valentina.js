5af57a2cf9339b8cfc7c708f12ca65bf67afc7ac
valentinaguti <valentina.gutierrez.gonzalez@alumnos.sip.cl>
2022-04-12 11:10:27 -0400

    diagrama en t

---
+++ valentina.js
@@ -0,0 +1,39 @@
+/*
+Fragmento de Codigo 1
+*/
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+variable datos
+produce  =  ["Marzanas", "naranjas"];
+/*
+variable datos 
+Produce  =  ["paula", "smith", "calle principal 1234", "st.louis", "MO", 12345];
+*/
+}
+
+
+//Fragmento de codigo 2
+
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+    /*
+    variable  Datos
+    Produce  =  ["manzanas", "Naranjas"]; 
+    Frozen  =  ["brocoli", "helado"];
+    */
+}
+/* 
+fragmento de codigo 3
+*/
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+/*
+   variables  Datos 
+   movielibrary  =  ["Bambi", "E:T", "Toy Story"];
+/*
